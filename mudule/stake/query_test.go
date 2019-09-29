ba5bf9544901a0052fabcf1be10f608e75e70b48
yangchuang64 <ytf2737179@163.com>
2019-09-29 17:08:54 +0800

    增加validator列表，validator总bond token查询

--- mudule/stake/query_test.go
+++ mudule/stake/query_test.go
@@ -1,4 +1,4 @@
-package mint
+package stake
 
 import (
 	"github.com/QOSGroup/qbase/client/types"
@@ -7,12 +7,13 @@ import (
 	"testing"
 )
 
-func TestQueryInflationPhrases(t *testing.T) {
+func TestQueryValidators(t *testing.T) {
 	viper.Set(types.FlagNode, "39.97.234.227:26657")
 	viper.Set(types.FlagNonceNode, "39.97.234.227:26657")
-	viper.Set(types.FlagNonce, 0)
+	viper.Set(types.FlagHeight, 0)
+	viper.Set(types.FlagTrustNode, true)
 
-	Tout, err := QueryInflationPhrases(codec.Cdc)
+	Tout, err := QueryValidators(codec.Cdc)
 	if err != nil {
 		t.Log(err)
 		return
@@ -21,26 +22,13 @@ func TestQueryInflationPhrases(t *testing.T) {
 	t.Log(string(bytes))
 }
 
-func TestQueryTotal(t *testing.T) {
+func TestQueryTotalValidatorBondToken(t *testing.T) {
 	viper.Set(types.FlagNode, "39.97.234.227:26657")
 	viper.Set(types.FlagNonceNode, "39.97.234.227:26657")
-	viper.Set(types.FlagNonce, 0)
+	viper.Set(types.FlagHeight, 0)
+	viper.Set(types.FlagTrustNode, true)
 
-	Tout, err := QueryTotal(codec.Cdc)
-	if err != nil {
-		t.Log(err)
-		return
-	}
-	bytes, err := codec.Cdc.MarshalJSON(Tout)
-	t.Log(string(bytes))
-}
-
-func TestQueryApplied(t *testing.T) {
-	viper.Set(types.FlagNode, "39.97.234.227:26657")
-	viper.Set(types.FlagNonceNode, "39.97.234.227:26657")
-	viper.Set(types.FlagNonce, 0)
-
-	Tout, err := QueryApplied(codec.Cdc)
+	Tout, err := QueryTotalValidatorBondToken(codec.Cdc)
 	if err != nil {
 		t.Log(err)
 		return
